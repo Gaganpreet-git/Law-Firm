382b925ed327c7f9570848e5ee961052c7c5b79d
Gaganpreet Singh <dmgaganpreet@gmail.com>
2023-10-21 18:02:57 +0530

    Button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Button from "./components/Button/Button";
 import Logo from "./components/Logo/Logo";
 
 function App() {
   return (
     <div className="App">
       <Logo />
+      <Button>Contact Now</Button>
     </div>
   );
 }
