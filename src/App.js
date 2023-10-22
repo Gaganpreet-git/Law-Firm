b6312535e359722aae8bc573b1d275ec2f8f8742
Gaganpreet Singh <dmgaganpreet@gmail.com>
2023-10-22 12:53:43 +0530

    Card

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import Card from "./components/Card/Card";
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
 import Intro from "./components/Intro/Intro";
@@ -6,8 +7,31 @@ import Intro from "./components/Intro/Intro";
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Hero />
+      <div className="hero-wrapper">
+        <Header />
+        <Hero />
+      </div>
+      <Card>
+        Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
+        sint. Velit officia consequatduis enim velit mollit Exer.Amet minim
+        mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit
+        officia consequatduis enim velit mollit Exer. Amet minim mollit non
+        deserunt ullamco est sit aliqua dolor do amet sint. Velit officia
+        consequatduis enim velit mollit Exer.Amet minim mollit non deserunt
+        ullamco est sit aliqua dolor do amet sint. Velit officia consequatduis
+        enim velit mollit Exer. Amet minim mollit non deserunt ullamco est sit
+        aliqua dolor do amet sint. Velit officia consequatduis enim velit mollit
+        Exer.Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
+        sint. Velit officia consequatduis enim velit mollit Exer. Amet minim
+        mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit
+        officia consequatduis enim velit mollit Exer.Amet minim mollit non
+        deserunt ullamco est sit aliqua dolor do amet sint. Velit officia
+        consequatduis enim velit mollit Exer. Amet minim mollit non deserunt
+        ullamco est sit aliqua dolor do amet sint. Velit officia consequatduis
+        enim velit mollit Exer.Amet minim mollit non deserunt ullamco est sit
+        aliqua dolor do amet sint. Velit officia consequatduis enim velit mollit
+        Exer.
+      </Card>
       <Intro />
     </div>
   );
