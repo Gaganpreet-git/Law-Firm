34bec78bc9619fec6e744658e876818d1420878f
Gaganpreet Singh <dmgaganpreet@gmail.com>
2023-10-22 14:34:09 +0530

    Card Updated ,added feaured class.

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import styles from "./Card.module.css";
 
-const Card = ({ children }) => {
-  return <div className={styles.card}>{children}</div>;
+const Card = ({ children, featured }) => {
+  const classes = `${styles.card}  ${featured ? styles.featured : ""}`;
+
+  return <div className={classes}>{children}</div>;
 };
 
 export default Card;
