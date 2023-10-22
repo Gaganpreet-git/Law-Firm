483121ba45fd3a199d799a3642cd8017258b23d9
Gaganpreet Singh <dmgaganpreet@gmail.com>
2023-10-22 14:47:42 +0530

    FeatureCard

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import Card from "./components/Card/Card";
+import FeatureCard from "./components/FeatureCard/FeatureCard";
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
 import Intro from "./components/Intro/Intro";
@@ -11,28 +11,14 @@ function App() {
         <Header />
         <Hero />
       </div>
-      <Card>
-        Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
-        sint. Velit officia consequatduis enim velit mollit Exer.Amet minim
-        mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit
-        officia consequatduis enim velit mollit Exer. Amet minim mollit non
-        deserunt ullamco est sit aliqua dolor do amet sint. Velit officia
-        consequatduis enim velit mollit Exer.Amet minim mollit non deserunt
-        ullamco est sit aliqua dolor do amet sint. Velit officia consequatduis
-        enim velit mollit Exer. Amet minim mollit non deserunt ullamco est sit
-        aliqua dolor do amet sint. Velit officia consequatduis enim velit mollit
-        Exer.Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
-        sint. Velit officia consequatduis enim velit mollit Exer. Amet minim
-        mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit
-        officia consequatduis enim velit mollit Exer.Amet minim mollit non
-        deserunt ullamco est sit aliqua dolor do amet sint. Velit officia
-        consequatduis enim velit mollit Exer. Amet minim mollit non deserunt
-        ullamco est sit aliqua dolor do amet sint. Velit officia consequatduis
-        enim velit mollit Exer.Amet minim mollit non deserunt ullamco est sit
-        aliqua dolor do amet sint. Velit officia consequatduis enim velit mollit
-        Exer.
-      </Card>
       <Intro />
+      <FeatureCard
+        featured
+        heading={"98% Success Rate"}
+        description={
+          "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequatduis enim velit mollit Exer."
+        }
+      />
     </div>
   );
 }
---
+++ src/components/FeatureCard/FeatureCard.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import styles from "./FeatureCard.module.css";
+import Card from "../Card/Card";
+import Button from "../Button/Button";
+import { ReactComponent as CardImage } from "../assets/gift-icon.svg";
+
+const FeatureCard = ({ heading, description, featured }) => {
+  return (
+    <div className={styles.featureCard}>
+      <Card featured={featured}>
+        <CardImage />
+        <h4 className={styles.heading}>{heading}</h4>
+        <p className={styles.description}>{description}</p>
+        <div className={styles.btn}>
+          <Button round>Read More</Button>
+        </div>
+      </Card>
+    </div>
+  );
+};
+
+export default FeatureCard;
